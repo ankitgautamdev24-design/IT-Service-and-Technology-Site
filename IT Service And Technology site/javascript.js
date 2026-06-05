7f6a25f216ac70c93159cc8b21719d5fedc75ce0
ankitgautamdev24-design <ankitgautam.dev24@gmail.com>
2026-06-05 11:53:19 +0530

    Add files via upload

---
+++ IT Service And Technology site/javascript.js
@@ -0,0 +1,15 @@
+// =========hamberger==========
+document.addEventListener("DOMContentLoaded", function () {
+
+  const hamburger = document.getElementById("hamburger");
+  const navMenu = document.getElementById("navMenu");
+
+  if (hamburger && navMenu) {
+    hamburger.addEventListener("click", function () {
+      navMenu.classList.toggle("active");
+    });
+  } else {
+    console.error("Hamburger or NavMenu not found in DOM");
+  }
+
+});
